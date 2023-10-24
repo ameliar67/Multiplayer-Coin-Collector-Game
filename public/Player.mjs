f5fb58d79fa5e2033174dfb3efe275aede91791f
Amelia <ameliar67@hotmail.com>
2023-10-23 18:37:50 -0700

    Add Player.mjs code

--- public/Player.mjs
+++ public/Player.mjs
@@ -1,18 +1,43 @@
 class Player {
-  constructor({x, y, score, id}) {
-
+  constructor(x, y, c_x, c_y, score, id) {
+    this.x = x;
+    this.y = y;
+    this.score = score;
+    this.id = id;
+    this.c_x = c_x;
+    this.c_y = c_y;
   }
 
   movePlayer(dir, speed) {
-
+    if (dir == 'x')  {
+   this.x = this.x + speed
+    } else  {
+   this.y = this.y + speed
+  }
   }
 
   collision(item) {
-
+    this.c_x = Math.floor(Math.random() * 500);
+    this.c_y = Math.floor(Math.random() * 500);
+    this.score = this.score + 1
   }
 
   calculateRank(arr) {
+    var length = arr.length
+    let sorted = arr.sort((p1, p2) => (p1.value > p2.value) ? 1 : (p1.value < p2.value) ? -1 : 0);
+    var idd = this.id
+    var sscore = this.score
+    var result = 0
 
+    var i;
+    for (i = 0; i < arr.length; i++)  {
+      if(arr[i].name == idd && arr[i].value > 0)  {
+        result = i
+        result++
+        break
+      }
+    }
+    return `Rank: ${result}/${length}`
   }
 }
 
