40d0c413b65d65cd65a851a2eb803087107f1f64
Amelia <ameliar67@hotmail.com>
2023-10-24 17:09:49 -0700

    Add alert message for incorrect key input

--- public/game.mjs
+++ public/game.mjs
@@ -15,18 +15,17 @@ document.addEventListener('keydown', (event) => {
   if (name == 'ArrowLeft') {
     player.movePlayer('x', -10)
     socket.emit('chat message', player.x);
-  } else if (name == 'ArrowRight'){
+  } else if (name == 'ArrowRight') {
     player.movePlayer('x', 10)
     socket.emit('chat message', player.y);
-  }  else if (name == 'ArrowUp')  {
+  } else if (name == 'ArrowUp') {
     player.movePlayer('y', -10)
     socket.emit('chat message', player.y);
-  }  else if (name == 'ArrowDown')  {
+  } else if (name == 'ArrowDown') {
     player.movePlayer('y', 10)
     socket.emit('chat message', player.y);
-  }  else  
-  {
-    alert(`${name} \n Key code Value: ${code}`);
+  } else {
+    alert(`Incorrect input: Please use arrow keys to navigate`);
   }
 }, false);
 
@@ -34,67 +33,67 @@ document.addEventListener('keydown', (event) => {
 var num = 200
 var num_y = 100
 
-function draw(num, num_y, score)  {
-context.clearRect(0,0,canvas.width,canvas.height);
+function draw(num, num_y, score) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
-context.beginPath();
-context.arc(player.c_x, player.c_y, 10, 0, 2 * Math.PI);
-context.strokeStyle = '#ff0000';
-context.stroke()
-context.fillStyle = "gold";
-context.fill();
-
-
-if (player.x >= (player.c_x - 31) && player.x <= (player.c_x + 31) && player.y >= (player.c_y - 31) && player.y <= (player.c_y + 31))  {
-  //alert(`Same!, ${player.c_x}, ${player.c_y}, ${player.x}, ${player.y}, ${JSON.stringify(ids)}`)
-  player.collision(0)
-  score++
-  socket.emit('chat message', (player.c_x, player.c_y));
-}
-
-context.beginPath();
-context.arc(player.x, player.y, 30, 0, 2 * Math.PI);
-context.strokeStyle = '#ff0000';
-context.stroke()
-context.fillStyle = "white";
-context.fill();
-
-context.beginPath();
-context.arc(player.x + 10, player.y - 10, 5, 0, 2 * Math.PI);
-context.fillStyle = "green";
-context.fill();
-
-context.beginPath();
-context.arc(player.x - 10, player.y - 10, 5, 0, 2 * Math.PI);
-context.fillStyle = "green";
-context.fill();
-
-context.beginPath();
-context.arc(player.x, player.y + 4, 15, 0.15, 3);
-context.fillStyle = "red";
-context.fill();
+  context.beginPath();
+  context.arc(player.c_x, player.c_y, 10, 0, 2 * Math.PI);
+  context.strokeStyle = '#ff0000';
+  context.stroke()
+  context.fillStyle = "gold";
+  context.fill();
 
 
-document.getElementById("score").innerHTML =     player.calculateRank(ids)
+  if (player.x >= (player.c_x - 31) && player.x <= (player.c_x + 31) && player.y >= (player.c_y - 31) && player.y <= (player.c_y + 31)) {
+    //alert(`Same!, ${player.c_x}, ${player.c_y}, ${player.x}, ${player.y}, ${JSON.stringify(ids)}`)
+    player.collision(0)
+    score++
+    socket.emit('chat message', (player.c_x, player.c_y));
+  }
+
+  context.beginPath();
+  context.arc(player.x, player.y, 30, 0, 2 * Math.PI);
+  context.strokeStyle = '#ff0000';
+  context.stroke()
+  context.fillStyle = "white";
+  context.fill();
+
+  context.beginPath();
+  context.arc(player.x + 10, player.y - 10, 5, 0, 2 * Math.PI);
+  context.fillStyle = "green";
+  context.fill();
+
+  context.beginPath();
+  context.arc(player.x - 10, player.y - 10, 5, 0, 2 * Math.PI);
+  context.fillStyle = "green";
+  context.fill();
+
+  context.beginPath();
+  context.arc(player.x, player.y + 4, 15, 0.15, 3);
+  context.fillStyle = "red";
+  context.fill();
+
+
+  document.getElementById("score").innerHTML = player.calculateRank(ids)
 }
 
 
 draw(num, num_y, score)
 
 
-socket.on('chat message', function(msg) {
+socket.on('chat message', function (msg) {
   player.id = socket.id
   //let index = ids.indexOf(socket.id)
   let obj = ids.find((o, i) => {
-      if (o.name === socket.id) {
-          ids[i] = { name: socket.id, value: player.score };
-          return true; // stop searching
-      }
-    
-  });
-  
-  if (obj == undefined)  {
-    ids.push({name: socket.id, value: player.score})
+    if (o.name === socket.id) {
+      ids[i] = { name: socket.id, value: player.score };
+      return true; // stop searching
     }
-  draw(num, num_y, score)
+
   });
+
+  if (obj == undefined) {
+    ids.push({ name: socket.id, value: player.score })
+  }
+  draw(num, num_y, score)
+});
--- server.js
+++ server.js
@@ -32,20 +32,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //For FCC testing purposes and enables user to connect from outside the hosting platform
-app.use(cors({origin: '*'})); 
+app.use(cors({ origin: '*' }));
 
 // Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+  });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-    
+
 // 404 Not Found Middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
@@ -58,9 +58,7 @@ var io = require('socket.io').listen(server);
 // Set up server and tests
 server.listen(portNum, "0.0.0.0", () => {
   console.log(`Listening on port ${portNum}`);
-
 });
-
 
 
 io.on('connection', (socket) => {
